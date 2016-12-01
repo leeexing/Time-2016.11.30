0310af3b3e48243390129dafd16555bf35b9b330
leeexing <674023427@qq.com>
2016-12-01 10:21:57 +0800

    gulp file 文件提交

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lixing1 on 2016/11/29.
+ */
+
+$(function(){
+    //
+    $(".glyphicon-chevron-down").click(function(){
+        $(".panel-two").toggleClass("collapse");
+    });
+
+});
