5f63de94c0837b6efba7b773d09b9015d83ea586
leeexing <lixingyaha@163.com>
2018-03-14 10:56:37 +0800

    FORMDATA_KOA is end.koa-multer is not good in upload file

--- javascript/formData/koa-app.js
+++ javascript/formData/koa-app.js
@@ -2,8 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 // koa 依赖模块
 const Router = require('koa-router')
-// const router = Router()
-const router = require('koa-better-router')().loadMethods()
+const router = Router()
 const server = require('koa-static')
 const body = require('koa-better-body')
 const bodyParser = require('koa-bodyparser')
@@ -17,34 +16,68 @@ const monk = require('monk')
 const monkUrl = 'localhost:27017/myblog'
 const db = monk(monkUrl)
 
+// koa app 中间件
 app.use(server(__dirname + '/static'))
 app.use(bodyParser())
 
-/*
-* 路由
-*/
+// 跨域设置
+app.use(async (ctx, next) => {
+  ctx.set({
+    "Access-Control-Allow-Origin": "http://localhost:8002", // 设置可以访问的域名端口
+    // 'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+    // "Access-Control-Allow-Headers": "X-Requested-With",
+    // 'Access-Control-Allow-Headers': 'Content-Type'
+  })
+  await next() // 必须要有“ await ”
+})
+// 统一返回数据的格式
+let result_data = {
+  success: true,
+  msg: 'ok',
+  data: null
+}
+/************************ 路由 *********************************/
+
 // 首页
 const index = async ctx => {
   ctx.body = fs.readFileSync(__dirname + '/index_koa.html', 'utf-8')
 }
-// 获取图像
-const getimg = async ctx => {
+// 获取一张图像
+const get_img = async ctx => {
   console.log(ctx.params)
-  db.get('users').find({}, {limit: 5}).then(data => {
-    console.log(data)
-  })
-  ctx.body = 'get image ~'
+  let {id} = ctx.params
+  if (!id) {
+    ctx.body = result_data
+    return
+  }
+  let data = await db.get('formData').findOne({imgMD5: id})
+  if (!data) {
+    data = await db.get('formData').findOne({})
+  }
+  result_data.data = data
+  ctx.body = result_data
+}
+// 获取所有图像
+const get_imgs = async ctx => {
+  let data = await db.get('formData').find({})
+  result_data.data = data
+  ctx.body = result_data
 }
 // 上传
 const upload = async ctx => {
-  console.log(ctx.req.body)
-  ctx.body = 'upload!'
+  console.log(ctx)
+  result_data.data = 'upload'
+  ctx.body = result_data
 }
 
 router.get('/', index)
-router.get('/getimg', getimg)
-router.post('/upload', body(), upload)
+router.get('/api/image/:id', get_img)
+router.get('/api/images', get_imgs)
+router.post('/api/upload', upload)
+
 app.use(router.routes())
+
+/******************** 启动 ********************************/
 
 app.listen(8002, () => {
   console.log('koa server listen 8002 ...')
--- javascript/formData/static/main.js
+++ javascript/formData/static/main.js
@@ -1,31 +1,87 @@
-/**
- * 如何使用 formdata
- * 
- */
+let btn = document.querySelector('.upload')
+let getImg = document.querySelector('.getimg')
+let getImgs = document.querySelector('.getimgs')
+let deleteImg = document.querySelector('.delete')
+let file = document.querySelector('#file')
+let imgWrap = document.querySelector('.img-wrap')
+let imgElem = document.querySelector('.img-wrap img')
 
-/* 
-let formData = new FormData()
-// formData主要有两个方法set，append
-formData.set('a', 5) // 把a的键值设置为5
-formData.append('b', 5) // 把b的兼职设置为5
+btn.onclick = () => {
+  file.click()
+}
 
-// 那set和append区别？？
-formData.set('a', 5)
-formData.set('a', 6) // 把a的键值设置为6   {a: 6}
-formData.append('b', 5)
-formData.append('b', 6) // 把b的键值[5, 6]  {b: [5, 6]} */
+file.onchange = e => {
+  console.info(e.target.files)
+  let files = Array.from(e.target.files)
+  query('http://localhost:8002/api/upload', 'post', files[0])
+    .then(data => {
+      console.log(data)
+    })
+  // upload(files)
+}
+
+getImg.onclick = e => {
+  // query('http://127.0.0.1:8002/api/image/33490c115c448295a842bc170b5dbc38').then(data => {
+  query('http://127.0.0.1:8002/api/image/0').then(data => {
+    console.log(data)
+  })
+}
+
+getImgs.onclick = () => {
+  query('http://127.0.0.1:8002/api/images').then(data => {
+    console.log(data)
+  })
+}
+
+deleteImg.onclick = () => {
+  delete_img()
+}
+
+function delete_img() {
+  let xhr = new XMLHttpRequest()
+  // xhr.open('delete', '/clearimg', true)
+  xhr.open('get', '/test/upload', true)
+  xhr.onload = function() {
+    if (this.status == 200) {
+      console.log(this.response)
+    }
+  }
+  xhr.send()
+}
 
 
-// Form的enctype属性
+function upload(files) {
+  console.log(files)
+  let formData = new FormData()
+  let xhr = new XMLHttpRequest()
+  Array.from(files).forEach(file => {
+    formData.append('fileName', file)
+  })
+  // formData.set('fileName', files)
+  xhr.open('POST', '/upload', true)
+  xhr.onreadystatechange =function() {
+    if (xhr.readyState == 4 && xhr.status === 200) {
+      console.log(this.response)
+    }
+  }
+  xhr.onerror = e => {
+    console.error(e)
+  }
+  xhr.send(formData)
+}
 
-// enctype这个属性管理的是表单的MIME编码，它一共有三个属性：
-// application/x-www-form-urlencoded
-// 在发送前编码所有字符（默认）
-
-
-// multipart/form-data
-// 不对字符编码，用来制定传输数据的特殊类型，如mp3、jpg
-
-
-// text/plain
-// 纯文本传输
+function query(url, type='get', data=null) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open(type, url, true)
+    xhr.onload = function(){
+      if (this.status === 200) {
+        resolve(JSON.parse(this.response))
+      }
+    }
+    xhr.onerror = err => {
+      reject(err)
+    }
+    xhr.send(data)
+  })
+}
