2a5bc02de20d70c6e56abc7db804fa15e956251c
leeexing <lixingyaha@163.com>
2018-07-05 12:31:06 +0800

    feat($view): Add css3 var demo - card

--- javascript/RegExp/test.js
+++ javascript/RegExp/test.js
@@ -3,3 +3,22 @@
  * 
  * 可以通过 RegExp.$1[,$2] 获取到匹配的数据
  */
+let arr = [1,2,3,4]
+arr.next = function(a, b, c) {
+  console.log(this)
+  this.push({
+    fn: a,
+    dir: b,
+    last: c
+  })
+  return arr
+}
+let a = arr.shift()
+console.log(a)
+
+const key = 'leeing_token'
+let key_en = new Buffer(key).toString('base64')
+console.log(key_en)
+let key_de = new Buffer(key_en, 'base64').toString()
+console.log(key_de)
+
