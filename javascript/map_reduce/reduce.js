01112f3fde7eb24f535722cd6d1594fa7a130913
leeexing <lixingyaha@163.com>
2018-09-03 10:08:54 +0800

    :memo: ($docs) 完善reduce的一些新使用技巧

--- javascript/map_reduce/reduce.js
+++ javascript/map_reduce/reduce.js
@@ -90,3 +90,100 @@ const grouping = (isValid, arr) =>
       isValid(next) ? [[...pass, next], fail] : [pass, [...fail, next]]
   )
 // 点评：太强悍了
+
+// !提取数据 takeWhile
+const houses = [
+  "Eddard Stark",
+  "Catelyn Stark",
+  "Rickard Stark",
+  "Brandon Stark",
+  "Rob Stark",
+  "Sansa Stark",
+  "Arya Stark",
+  "Bran Stark",
+  "Rickon Stark",
+  "Lyanna Stark",
+  "Tywin Lannister",
+  "Cersei Lannister",
+  "Jaime Lannister",
+  "Tyrion Lannister",
+  "Joffrey Baratheon"
+]
+let ret = houses.filter(item => item.includes('Stark'))
+
+const takeWhile = f => ([head, ...tail]) =>
+  f(head) ? [head, ...takeWhile(f)(tail)] : []
+const isStark = name => name.toLowerCase().includes('stark')
+console.log(takeWhile(isStark)(houses))
+// 点评。takeWhile这个函数在其他地方也看到过，看起来还是很不错的
+
+// !使用takewhile控制合适停止。找出数组中的奇数，然后取出前4个
+const numList = [1, 3, 11, 4, 2, 5, 6, 7]
+const takeFirst = (limit, f, arr) => {
+  if (limit === 0 || arr.length === 0) {
+    return []
+  }
+  const [head, ...tail] = arr
+  return f(head) ? [head, ...takeFirst(limit - 1, f, tail)] : takeFirst(limit, f, tail)
+}
+const isOdd = num => num % 2 === 1
+console.log(takeFirst(4, isOdd, numList))
+// 点评：使用递归可以很好的控制函数的执行情况
+
+// !pipe.辅助函数
+// const pipe = (...fns) => (...args) => fns.reduce((fx, fy) => fy(fx), ...args)
+const numList1 = [1, 3, 11, 4, 2, 5, 6, 7]
+const add = x => x + 2
+const mul = y => y * 2
+const pipe = (...fns) => (...args) => fns.reduce((fx, fy) => fy(fx), ...args)
+let ret = numList1.map(pipe(add, mul))
+console.log(ret)
+// 点评：需要好好理解并适应且使用这种 -- 函数没有直接显示参数的表达方式
+
+// !一个高级一些的概念 -- Transduce
+const filter = (f, arr) =>
+  arr.reduce((acc, val) => (f(val) && acc.push(val), acc), [])
+
+const map = (f, arr) =>
+  arr.reduce((arr, val) => (arr.push(f(val)), acc), [])
+// 点评一：使用reduce来定义 filter 和 map。他们都有一定的共性
+
+filter = f => reducer => (acc, val) => {
+  if (f(val)) reducer(acc, val)
+  return acc
+}
+map = f => reducer => (acc, val) => reducer(acc, f(val))
+// 点评二：将共通之处提取出来
+
+const pushReducer = (acc, val) => (acc.push(val), acc)
+const addReducer = (acc, val) => acc + val
+
+const isEven = x => x % 2 === 0
+const add2 = x => x + 2
+
+console.log('test1', numList.reduce(filter(isEven)(pushReducer), []) )
+console.log('test2', numList.reduce(map(add2)(addReducer)) )
+// 点评三：这种思维还是很新颖。需要消化并在工作中常用
+
+// !使用黑科技.很有意思的一个实现
+function *repeatArr (arr) {
+  let i = 0
+  let len = arr.length
+  while(i < len) {
+    yield arr[i++]
+  }
+}
+const infiniteNameList = repeatArr(starks)
+const wait = ms =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+const fn = async () => {
+  for (let name of infiniteNameList) {
+    await wait(2000)
+    console.log(name)
+  }
+}
+console.log(fn())
